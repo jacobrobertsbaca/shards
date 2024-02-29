3292831cafab8294b8a3926a5c8172b6910e1644
JTRB <jacobrobertsbaca@gmail.com>
2024-02-28 22:21:37 -0800

    feat: TagSerializer interface

---
+++ src/Packages/com.shards/Runtime/Tags/TagSerializer.cs
@@ -0,0 +1,22 @@
+﻿namespace Shards.Tags
+{
+    public abstract class TagSerializer<T>
+    {
+        public abstract bool TryDeserialize(ITag tag, out T value);
+        public abstract ITag Serialize(T value);
+    }
+
+    public static class TagSerializer
+    {
+        public static bool TryDeserialize<T>(ITag tag, out T value)
+        {
+            value = default;
+            return false;
+        }
+
+        public static ITag Serialize<T>(T value)
+        {
+            return null;
+        }
+    }
+}
